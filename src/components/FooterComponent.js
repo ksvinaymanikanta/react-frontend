03aec6b6ee83cfa20c5c20b22076fd2acd76181c
K.S.vinay <22P31F0023@acet.ac.in>
2025-01-31 18:22:13 +0530

    my frontend

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css';
+
+function FooterComponent() {
+  return (
+        <footer className='footer'>
+            <small>All Right Reserved &copy; SIDDU</small>
+        </footer>
+  )
+}
+
+export default FooterComponent
